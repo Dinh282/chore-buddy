e8e8ef47018bf837aba628559d5d5aedf0055bea
Felix W <felixwillem01@yahoo.com>
2023-08-27 19:29:33 -0400

    worked on dashboard logic, debugging

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { Col, Row } from 'antd';
 import { useCurrentUserContext } from '../context/CurrentUser';
-
 import Parent from '../components/Parent/Parent';
 import Child from '../components/Child/';
 import { useQuery } from '@apollo/client';
@@ -8,16 +7,15 @@ import { QUERY_CURRENT_USER } from '../graphql/queries'
 
 function Dashboard() {
   const { currentUser } = useCurrentUserContext();
-
   const { loading, data } = useQuery(QUERY_CURRENT_USER, {
     variables: { email: currentUser.email }
   })
+
    if (loading) {
     return <div>Loading...</div>;
   }
   console.log('data>>>>.', data)
   const user = data?.getCurrentUser || []
-  // console.log('cureentuserisChoreBuddy>>>>>', user.isChoreBuddy)
 
  
   return (
